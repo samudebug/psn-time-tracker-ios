cfece4de5db2e1d38777cf619111ef91ecf36f0c
Samuel Martins <samuel.ubuntu@gmail.com>
2023-09-30 16:17:43 -0300

    Initial Commit

---
+++ PSN Time Tracker/PSN_Time_TrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  PSN_Time_TrackerApp.swift
+//  PSN Time Tracker
+//
+//  Created by Samuel Martins on 30/09/23.
+//
+
+import SwiftUI
+
+@main
+struct PSN_Time_TrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
